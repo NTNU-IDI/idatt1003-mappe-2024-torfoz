90aafbb5a8d740ad6fa932b4d50a43253f6abb9b
torfoz <tord.fosse@gmail.com>
2024-11-04 18:01:23 +0100

    Metoder for å legge til og ta ingredienser fra ArrayList

--- src/main/java/edu/ntnu/iir/bidata/Fridge.java
+++ src/main/java/edu/ntnu/iir/bidata/Fridge.java
@@ -1,3 +1,38 @@
 package edu.ntnu.iir.bidata;
 
-public class Fridge {}
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fridge {
+  private List<Ingredient> ingredients = new ArrayList<>();
+
+  public void addIngredient(
+      String name, double amount, Unit unit, LocalDate expiryDate, double price) {
+    for (Ingredient ingredient : ingredients) {
+      if (ingredient.getName().equals(name)
+          && ingredient.getAmount() == amount
+          && ingredient.getUnit() == unit
+          && ingredient.getExpiryDate().equals(expiryDate)
+          && ingredient.getPrice() == price) {
+        ingredient.setQuantity(ingredient.getQuantity() + 1);
+        return;
+      }
+    }
+    ingredients.add(new Ingredient(1, name, amount, unit, expiryDate, price));
+  }
+
+  public List<Ingredient> getIngredients() {
+    return ingredients;
+  }
+
+  public List<Ingredient> getIngredientsByName(String name) {
+    List<Ingredient> ingredientsByName = new ArrayList<>();
+    for (Ingredient ingredient : ingredients) {
+      if (ingredient.getName().equals(name)) {
+        ingredientsByName.add(ingredient);
+      }
+    }
+    return ingredientsByName;
+  }
+}
