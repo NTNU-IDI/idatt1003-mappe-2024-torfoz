bf9f199dcb4af3a75dc1b9fce9947b2a7c6603b7
torfoz <tord.fosse@gmail.com>
2024-12-04 23:25:34 +0100

    method for getting recipe type

--- src/main/java/edu/ntnu/iir/bidata/utils/InputUtil.java
+++ src/main/java/edu/ntnu/iir/bidata/utils/InputUtil.java
@@ -1,6 +1,7 @@
 package edu.ntnu.iir.bidata.utils;
 
-import edu.ntnu.iir.bidata.Unit;
+import edu.ntnu.iir.bidata.enums.RecipeType;
+import edu.ntnu.iir.bidata.enums.Unit;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
@@ -73,6 +74,33 @@ public class InputUtil {
         return LocalDate.parse(scanner.nextLine(), format);
       } catch (Exception e) {
         System.out.println("Invalid input. Please enter a date in the format dd-MM-yyyy.");
+      }
+    }
+  }
+
+  public static RecipeType getRecipeType() {
+    while (true) {
+      System.out.print(
+          "Recipe type (BREAKFAST, LUNCH, STARTER, DINNER, DESSERT): ");
+      String typeString = scanner.nextLine().toUpperCase();
+      try {
+        return RecipeType.valueOf(typeString);
+      } catch (IllegalArgumentException e) {
+        System.out.println("Invalid recipe type: " + typeString + ". Please enter a valid recipe type.");
+      }
+    }
+  }
+
+  public static boolean getBoolean(String prompt) {
+    while (true) {
+      System.out.print(prompt);
+      String input = scanner.nextLine();
+      if (input.equalsIgnoreCase("yes")) {
+        return true;
+      } else if (input.equalsIgnoreCase("no")) {
+        return false;
+      } else {
+        System.out.println("Invalid input. Please enter yes or no.");
       }
     }
   }
