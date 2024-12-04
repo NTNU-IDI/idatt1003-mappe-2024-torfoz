acbbefafda36d579c6b1b2f08224b9fbe1d837e8
torfoz <tord.fosse@gmail.com>
2024-12-04 18:43:04 +0100

    Refactored Ingredient to Grocery

--- src/test/java/FridgeTest.java
+++ src/test/java/FridgeTest.java
@@ -8,25 +8,34 @@ public class FridgeTest {
   @Test
   public void testGetters() {
     Fridge fridge = new Fridge();
-    fridge.addIngredient("Egg", 1, Unit.PIECE, LocalDate.now(), 5);
-    assertEquals(1, fridge.getIngredientsInFridge().size());
+    fridge.addGrocery("Egg", 1, Unit.PIECE, LocalDate.now(), 5);
+    assertEquals(1, fridge.getGroceriesInFridge().size());
   }
 
   @Test
-  public void testRemoveIngredientFromFridge() {
+  public void testAddGroceryFromInput() {
     Fridge fridge = new Fridge();
-    fridge.addIngredient("Egg", 1, Unit.PIECE, LocalDate.now(), 5);
-    fridge.addIngredient("Egg", 1, Unit.PIECE, LocalDate.now(), 10);
+    String simulatedUserInput = "Egg\n1\nstk\n13-12-2024\n5\n";
+    System.setIn(new java.io.ByteArrayInputStream(simulatedUserInput.getBytes()));
+    fridge.addGroceryFromInput();
+    assertEquals(1, fridge.getGroceriesInFridge().size());
+  }
+
+  @Test
+  public void testRemoveGroceryFromFridge() {
+    Fridge fridge = new Fridge();
+    fridge.addGrocery("Egg", 1, Unit.PIECE, LocalDate.now(), 5);
+    fridge.addGrocery("Egg", 1, Unit.PIECE, LocalDate.now(), 10);
     String simulatedUserInput = "Egg\n1\n";
     System.setIn(new java.io.ByteArrayInputStream(simulatedUserInput.getBytes()));
-    fridge.removeIngredientFromFridge();
-    assertEquals(1, fridge.getIngredientsInFridge().size());
+    fridge.removeGroceryFromFridge();
+    assertEquals(1, fridge.getGroceriesInFridge().size());
   }
 
   @Test
   public void testToString() {
     Fridge fridge = new Fridge();
-    fridge.addIngredient("Egg", 1, Unit.PIECE, LocalDate.now(), 5);
+    fridge.addGrocery("Egg", 1, Unit.PIECE, LocalDate.now(), 5);
     String expected =
         "#######################\n"
             + "------ KJØLESKAP ------\n"
